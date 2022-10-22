88158bae593ca0edb891938514af26c696b25a2a
lich.chen <lich.chen@elitescloud.com>
2022-10-22 16:50:33 +0800

    feat(ws): 新增ws包

--- server/server/app.js
+++ server/server/app.js
@@ -7,8 +7,9 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/accounts');
+var expressWs = require('express-ws')
 var app = express();
-
+expressWs(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -17,8 +18,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-const baseRoute = "/api/v01";
 
 app.use( cors ({
   origin: "*",
@@ -26,9 +25,7 @@ app.use( cors ({
   preflightContinue: false,
   optionsSuccessStatus: 200
 }) );
-app.use(`${baseRoute}`, indexRouter);
-app.use(`${baseRoute}/users`, usersRouter);
-app.use(`${baseRoute}/accounts`, accountRouter);
+app.use(indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -44,5 +41,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(msg) {
+    ws.send(msg);
+  });
+});
 module.exports = app;
--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,6 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
+    createProxyMiddleware('/ws', {
+      target: 'http://localhost:9000', // 测试环境
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: {
+        '^/ws': ''
+      }
+    }),
     createProxyMiddleware('/yst-tms', {
       target: 'https://nrp-dev.elitescloud.com', // 测试环境
       changeOrigin: true,
